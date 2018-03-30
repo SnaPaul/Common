6e1bc27c5ffe10e53bb768f848121498839750ac
SnaPaul <snajder.paul@gmail.com>
2018-03-30 12:24:08 +0200

    Added C# field tutorial course VS project.

---
+++ C_Sharp_fields_tutorial/c_sharp_fields_tutorial/c_sharp_fields_tutorial/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_fields_tutorial
+{
+    public class Order
+    {
+
+    }
+}
