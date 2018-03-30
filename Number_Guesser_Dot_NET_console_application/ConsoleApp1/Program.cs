e8a3bf348356c29455a3adde4aaef825cdacd121
SnaPaul <snajder.paul@gmai.com>
2018-03-30 11:04:36 +0200

    First commit mit initial filesFirst commit mit initial files..

---
+++ Number_Guesser_Dot_NET_console_application/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppInfo();
+
+            GreetUser();
+
+            while (true)
+            {
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+
+                int guess = 0;
+
+                Console.WriteLine("Guess a number between 0 and 10.");
+
+                while (guess != correctNumber)
+                {
+                    string inputNumber = Console.ReadLine();
+
+                    if (!int.TryParse(inputNumber, out guess))
+                    {               
+                        PrintedColoredConsoleMessage(ConsoleColor.Yellow, "This is not a number!!!");
+                        continue;
+                    }
+
+
+                    guess = Int32.Parse(inputNumber);
+
+                    if (guess != correctNumber)
+                    {
+                        PrintedColoredConsoleMessage(ConsoleColor.Red, "Wrong number, please try again.");
+                    }
+                }
+
+                PrintedColoredConsoleMessage(ConsoleColor.Blue, "Right Number");
+
+                Console.WriteLine("");
+
+                PrintedColoredConsoleMessage(ConsoleColor.Cyan, "Like to play again?");
+                PrintedColoredConsoleMessage(ConsoleColor.Cyan, "Type Yes otherwise the game ends!!!");
+
+                string answer = Console.ReadLine().ToUpper();
+                Console.ResetColor();
+
+                if (answer != "YES")
+                {
+                    break;
+                }
+            }
+        }
+
+        static void AppInfo()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAutor = "Me Myself";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("{0}: Version {1} by {2}.", appName, appVersion, appAutor);
+
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your name?");
+
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", inputName);
+        }
+
+        static void PrintedColoredConsoleMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ markdown_course/markdown.md
@@ -0,0 +1,44 @@
+<!-- Headings -->
+
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+*This text* _is italic_
+
+**Strong text**
+
+__Strong text__
+
+
+~~this text is striked through~~
+
+> This is a quote
+
+[Link text](www.wikipedia.com "wikipedia")
+
+* Item 1
+* Item 2
+* Item 3
+	* Nested Item 3.1
+
+1. Item 1
+1. Item 2
+1. Item 3
+
+`<p> This is a paragraph </p>`
+
+```bash
+npm install
+
+npm start
+```
+
+```javascript
+fuction(){
+
+}
+```
